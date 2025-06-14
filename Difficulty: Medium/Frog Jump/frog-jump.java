be76ec092341dbe8a46925a807f905f4ec297357
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-06-14 15:49:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,24 +1,23 @@
 // User function Template for Java
 class Solution {
-    public int minCost(int[] height) {
+    int minCost(int[] height) {
         // code here
-        int dp[] = new int[height.length];
-        return minCost(height.length-1 , height,dp);
-        
+        int dp[] = new int[height.length+1];
+        Arrays.fill(dp , -1);
+        return solve(0 , height , dp);
     }
-    public int minCost(int indx , int[]height , int[]dp){
-        if(indx == 0){
+    public int solve(int indx , int height[] , int[]dp){
+        if(indx == height.length-1){
             return 0;
         }
-        if(dp[indx] != 0){
+        if(dp[indx] != -1){
             return dp[indx];
         }
-        
-        int jump1 = minCost(indx-1 , height,dp) + Math.abs(height[indx] - height[indx-1]);
+        int jump1 = Math.abs(height[indx] - height[indx+1]) + solve(indx+1,height,dp) ;
         int jump2 = Integer.MAX_VALUE;
-        if(indx > 1){
-            jump2 = minCost(indx-2, height,dp) + Math.abs(height[indx] - height[indx-2]);
+        if(indx < height.length-2){
+            jump2 = Math.abs(height[indx] - height[indx+2]) + solve(indx+2 , height,dp);
         }
-        return dp[indx]=Math.min(jump1 , jump2);
+        return dp[indx] = Math.min(jump1 , jump2);
     }
 }
