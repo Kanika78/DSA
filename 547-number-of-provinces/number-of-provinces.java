5ceb3b3ffd97e96766b530fbea77771dcec95712
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-07-23 16:18:55 +0530

    Time: 4 ms (17.57%) | Memory: 46.8 MB (88.61%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,49 +1,35 @@
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        int rank[] = new int[isConnected.length];
-        int parent[] = new int[isConnected.length];
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
 
-        for(int i=0 ; i<parent.length ; i++){
-            parent[i] = i;
-        }
         for(int i=0 ; i<isConnected.length ;i++){
-            for(int j=i+1 ; j<isConnected.length ; j++){
-                if(isConnected[i][j] == 1){
-                    union(i , j , parent, rank);
+            adj.add(new ArrayList<>());
+        }
+
+        for(int i=0; i<isConnected.length ; i++){
+            for(int j=0; j<isConnected.length ; j++){
+                if(i != j && isConnected[i][j] == 1){
+                    adj.get(i).add(j);
+                    adj.get(j).add(i);
                 }
             }
         }
-        int comp = 0;
-        for(int i=0 ; i<parent.length ; i++){
-            if(parent[i] == i){
-                comp++;
+        int count = 0;
+        int vis[] = new int[isConnected.length];
+        for(int i=0 ; i<adj.size() ; i++){
+            if(vis[i] == 0){
+                count++;
+            }
+            dfs(i , vis,adj);
+        }
+        return count;   
+    }
+    public void dfs(int curr,int[]vis,ArrayList<ArrayList<Integer>> adj){
+        vis[curr] = 1;
+        for(int neigh : adj.get(curr)){
+            if(vis[neigh] == 0){
+                dfs(neigh,vis,adj);
             }
         }
-        return comp;
     }
-    public boolean union(int u , int y, int[]parent, int[]rank){
-        int parent_u = find(u , parent);
-        int parent_y = find(y , parent);
-
-        if(parent_u == parent_y){
-            return false;
-        }
-        if(rank[parent_u] < rank[parent_y]){
-            parent[parent_u] = parent_y;
-        }
-        else if(rank[parent_u] > rank[parent_y]){
-            parent[parent_y] = parent_u;
-        }else{
-            parent[parent_u] = parent_y;
-            rank[parent_y]++;
-        }
-        return true;
-    }
-    public int find(int u, int[] parent) {
-        if (parent[u] != u) {
-            parent[u] = find(parent[u], parent);  
-        }
-        return parent[u];
-    }
-
 }
