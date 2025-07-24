f2e8314600be634c1ef88d2b7fe5c4f725c8cf1e
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-07-24 14:35:49 +0530

    Time: 1 ms (46.21%) | Memory: 45.4 MB (5.11%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,27 +1,22 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int ans[][] = new int[image.length][image[0].length];
         boolean vis[][] = new boolean[image.length][image[0].length];
-        for(int i=0 ; i<image.length;i++){
-            for(int j=0 ; j<image[0].length;j++){
-                ans[i][j] = image[i][j];
-            }
-        }
-        dfs(sr,sc,color,vis,ans,image);
-        return ans;
+        int original = image[sr][sc];
+        dfs(sr,sc,color,vis,image,original);
+        return image;
         
     }
-    public void dfs(int row,int col,int color,boolean[][]vis , int[][]ans, int[][]image){
+    public void dfs(int row,int col,int color,boolean[][]vis , int[][]image,int original){
         vis[row][col] = true;
-        ans[row][col] = color;
+        image[row][col] = color;
 
         int delrow[] = {-1,0,1,0};
         int delcol[] = {0,-1,0,1};
         for(int i=0;i<4;i++){
             int nRow = delrow[i]+row;
             int nCol = delcol[i]+col;
-            if(nRow <ans.length && nRow>=0 && nCol <ans[0].length && nCol>=0 && !vis[nRow][nCol] && image[row][col] == image[nRow][nCol]){
-                dfs(nRow,nCol,color,vis,ans,image);
+            if(nRow <image.length && nRow>=0 && nCol <image[0].length && nCol>=0 && !vis[nRow][nCol] &&image[nRow][nCol]==original){
+                dfs(nRow,nCol,color,vis,image,original);
 
             }
         }
