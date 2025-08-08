4bb04d23087ac062ef7e45bf62ff166995ea265e
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-08-08 18:23:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimize connections/minimize-connections.java
@@ -0,0 +1,55 @@
+class Solution {
+    public static int minimumConnections(int n, int[][] connections) {
+        // code here
+        int[] par = new int[n];
+        int[] rank = new int[n];
+        for(int i=0 ; i<n ; i++){
+            rank[i] = 0;
+            par[i] = i;
+        }
+        int edges = 0;
+        for(int edge[] : connections){
+            int u = edge[0];
+            int v = edge[1];
+            if(!UnionRank(u , v, par,rank)){
+                edges++;
+            } 
+        }
+        int noOfComponent = 0;
+        for(int i=0 ; i< n ; i++){
+            if(findPar(i, par)== i){
+                noOfComponent++;
+            }
+        }
+        return edges >= noOfComponent-1 ? noOfComponent-1 : -1; 
+
+        
+    }
+    public static int findPar(int u , int[]par){
+        if(u == par[u]){
+            return u;
+        }
+        return par[u] = findPar(par[u] , par);
+    }
+    public static boolean UnionRank(int u , int v , int par[] , int rank[]){
+        int ulp_u = findPar(u,par);
+        int ulp_v = findPar(v,par);
+        if(ulp_u == ulp_v){
+            return false;
+        }
+        if(rank[ulp_u] < rank[ulp_v]){
+            par[ulp_u] = par[ulp_v];
+            return true;
+        }
+        else if(rank[ulp_u] > rank[ulp_v]){
+            par[ulp_v] = par[ulp_u];
+            return true;
+        }else{
+            par[ulp_v] = par[ulp_u];
+            int rankU = rank[ulp_u];
+            rank[ulp_v] = rankU+1;
+            return true;
+
+        }
+    }
+}
