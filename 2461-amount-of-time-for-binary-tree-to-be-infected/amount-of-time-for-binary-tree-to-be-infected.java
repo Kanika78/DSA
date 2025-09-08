04b1601926cbde73439b0bff43e56ad191fa6e7a
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-09-08 14:27:06 +0530

    Time: 94 ms (65.92%) | Memory: 72.6 MB (72.73%) - LeetSync

---
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.java
@@ -0,0 +1,81 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void LevelOrder(TreeNode root , HashMap<TreeNode , TreeNode> map,int start){
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0 ; i<size ; i++){
+                TreeNode curr = q.remove();
+                if(curr.left != null){
+                    q.add(curr.left);
+                    map.put(curr.left ,curr);
+                }
+                if(curr.right != null){
+                    q.add(curr.right);
+                    map.put(curr.right ,curr);
+                }
+            }
+        }
+    }
+    public TreeNode findTarget(TreeNode root, int start){
+        if(root == null){
+            return null;
+        }
+        if(root.val== start){
+            return root;
+        }
+        TreeNode left = findTarget(root.left , start);
+        if(left != null){
+            return left;
+        }
+        return findTarget(root.right , start);
+    }
+    public int amountOfTime(TreeNode root, int start) {
+        HashMap<TreeNode , TreeNode> map = new HashMap<>();
+        LevelOrder(root , map,start);
+        Queue<TreeNode> q = new LinkedList<>();
+        HashMap<TreeNode , Boolean> vis = new HashMap<>();
+        TreeNode target = findTarget(root , start);
+        int min = 0;
+        q.add(target);
+        vis.put(target,true);
+
+        while(!q.isEmpty()){
+            min++;
+            int size = q.size();
+            for(int i=0 ; i<size ; i++){
+                TreeNode curr = q.remove();
+                if(curr.left != null && vis.get(curr.left) == null){
+                    q.add(curr.left);
+                    vis.put(curr.left , true);
+                }
+                if(curr.right != null && vis.get(curr.right) == null){
+                    q.add(curr.right);
+                    vis.put(curr.right , true);
+                }
+                if(map.get(curr) != null && vis.get(map.get(curr)) == null){
+                    q.add(map.get(curr));
+                    vis.put(map.get(curr) , true);
+                }
+            }
+
+        }
+        return min-1;
+        
+    }
+}
