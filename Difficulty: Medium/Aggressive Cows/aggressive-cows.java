3fb2e5fff278c8c18d11555fe1fa730e88b89aa1
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-10-23 14:37:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,46 +1,19 @@
-//{ Driver Code Starts
-// Initial Template for Java
-
-import java.io.*;
-import java.util.*;
-
-class GFG {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int tc = Integer.parseInt(br.readLine().trim());
-
-        while (tc-- > 0) {
-
-            String[] str = br.readLine().trim().split(" ");
-            int[] a = new int[str.length];
-            for (int i = 0; i < str.length; i++) {
-                a[i] = Integer.parseInt(str[i]);
-            }
-            String[] nk = br.readLine().trim().split(" ");
-            int k = Integer.parseInt(nk[0]);
-            Solution sln = new Solution();
-            int ans = sln.aggressiveCows(a, k);
-
-            System.out.println(ans);
-            System.out.println("~");
-        }
-    }
-}
-// } Driver Code Ends
-
-
-// User function Template for Java
 class Solution {
-    public static int aggressiveCows(int[] stalls, int k) {
+    public int aggressiveCows(int[] stalls, int k) {
         // code here
         Arrays.sort(stalls);
-        int n = stalls.length;
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i=0 ; i<stalls.length ; i++){
+            max = Math.max(max,stalls[i]);
+            min = Math.min(min,stalls[i]);
+        }
         int ans = 0;
-        int low = 1; //min distance btw cows
-        int high = stalls[n-1] - stalls[0];
+        int low = 1;
+        int high = max - min;
         while(low <= high){
-            int mid = (low + high)/2;
-            if(canPlace(stalls , mid , k)){
+            int mid = low + (high - low)/2;
+            if(canPlaceCow(stalls,mid,k) == true){
                 ans = mid;
                 low = mid + 1;
             }else{
@@ -48,18 +21,17 @@ class Solution {
             }
         }
         return ans;
-        
     }
-    public static boolean canPlace(int[] stalls,int mid, int k){
-        int cntcow = 1;
-        int lastIndx = stalls[0];
-        for(int i = 1 ; i < stalls.length ; i++){
-            if(stalls[i] - lastIndx >= mid){
-                cntcow++;
-                lastIndx = stalls[i];
+    public boolean canPlaceCow(int[] arr , int dist , int cows){
+        int cntCows = 1;
+        int lastIndx = arr[0];
+        for(int i=1 ; i<arr.length ; i++){
+            if(arr[i] - lastIndx >= dist){
+                cntCows++;
+                lastIndx = arr[i];
             }
         }
-        if(cntcow >= k){
+        if(cntCows >= cows){
             return true;
         }
         return false;
