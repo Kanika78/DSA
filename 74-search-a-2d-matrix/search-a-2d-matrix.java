a2ccf3c861e8716e19c5ac8f78f0e4e700fc2aa6
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-10-24 13:54:12 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (92.39%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -3,19 +3,19 @@ class Solution {
         int n = matrix.length;
         int m = matrix[0].length;
         int low = 0;
-        int high = n*m - 1;
+        int high = (n*m)-1;
+
         while(low <= high){
             int mid = (low + high)/2;
-            int row = mid / m;
+            int row = mid/m;
             int col = mid % m;
+
             if(matrix[row][col] == target){
                 return true;
-            }
-            else if(matrix[row][col] > target){
-                high = mid - 1;
-            }
-            else{
+            }else if(matrix[row][col] < target){
                 low = mid + 1;
+            }else{
+                high = mid - 1;
             }
         }
         return false;
