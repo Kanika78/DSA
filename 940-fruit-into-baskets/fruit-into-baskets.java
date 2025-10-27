bc72efdaca3c8a166a7988fbd2c05bcf2282494e
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-10-28 01:35:00 +0530

    Time: 57 ms (8.63%) | Memory: 55.7 MB (18.93%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -11,7 +11,7 @@ class Solution {
             }else{
                 map.put(fruits[r],1);
             }
-            while(map.size() > 2){
+            if(map.size() > 2){
                 map.put(fruits[l] , map.get(fruits[l])-1);
                 if(map.get(fruits[l]) == 0){
                     map.remove(fruits[l]);
