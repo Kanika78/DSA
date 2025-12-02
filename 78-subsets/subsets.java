c57585b0358b823e67ea5836b65e50265f77c927
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-12-02 14:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (33.22%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,23 +1,21 @@
 class Solution {
-    public void subsequence(int indx ,int nums[], List<List<Integer>> ls, List<Integer> ds){
-        if(indx >= nums.length){
-            ls.add(new ArrayList<>(ds));
-            return;
-        }
-        ds.add(nums[indx]);
-        subsequence(indx+1 , nums , ls ,ds);
-        ds.remove(ds.size()-1);
-        subsequence(indx+1 , nums , ls , ds);
-
-    }
     public List<List<Integer>> subsets(int[] nums) {
-        List<Integer> ds = new ArrayList<>();
-        List<List<Integer>> ls = new ArrayList<>();
-
-        subsequence(0 , nums , ls , ds);
-        return ls;
+        List<Integer> inner = new ArrayList<>();
+        List<List<Integer>> outer = new ArrayList<>();
+        helper(0 , inner , outer , nums);
+        return outer;
 
 
         
+    }
+    public void helper(int indx,List<Integer>inner ,List<List<Integer>> outer, int[] nums){
+        if(indx >= nums.length){
+            outer.add(new ArrayList<>(inner));
+            return;
+        }
+        inner.add(nums[indx]);
+        helper(indx+1 , inner , outer , nums);
+        inner.remove(inner.size()-1);
+        helper(indx+1 , inner , outer , nums);
     }
 }
