4fcfcc6e5767981cf8d379861c85fdd2d6190297
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2025-12-07 19:26:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.java
@@ -2,26 +2,35 @@ class Solution {
 
     static Boolean isSubsetSum(int arr[], int sum) {
         // code here
-        int sum1 = 0;
-        for(int i=0 ; i<arr.length ; i++){
-            sum1 += arr[i];
+        int dp[][] = new int[arr.length][sum+1];
+        for(int r[] : dp){
+            Arrays.fill(r,-1);
+            
         }
-        if(sum1 < sum){
-            return false;
-        }
-        boolean dp[] = new boolean[arr.length];
-        return subset(0 ,sum,arr,dp);
+        solve(arr.length-1,sum,arr,dp);
+        return dp[arr.length-1][sum] == 1;
+        
+        
     }
-    public static boolean subset(int indx ,int sum,int arr[],boolean[]dp){
-       if(indx == arr.length-1 && (sum == arr[indx] || sum == 0)){
-           return true;
-       }
-       if(sum < 0 || indx >= arr.length){
-           return false;
-       }
-       if(dp[indx] != false){
-           return dp[indx];
-       }
-       return dp[indx] = subset(indx+1 , sum - arr[indx] , arr,dp) || subset(indx+1 , sum, arr,dp);
+    public static boolean solve(int indx,int target,int[]arr,int[][]dp){
+        if(target == 0){
+            return true;
+        }
+        if(indx == 0){
+            return arr[0] == target;
+        }
+        if(dp[indx][target] != -1){
+            return dp[indx][target] == 1;
+        }
+        boolean notTake = solve(indx-1,target,arr,dp);
+        boolean take = false;
+        if(arr[indx] <= target){
+            take = solve(indx-1 , target-arr[indx],arr,dp);
+        }
+        dp[indx][target] = (take || notTake) ? 1 : 0;
+        return dp[indx][target] == 1;
+        
+        
+        
     }
 }
