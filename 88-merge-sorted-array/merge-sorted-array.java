48399a41380f39745bd5c1703276e3dbae6af024
Kanika Goyal <145834114+Kanika78@users.noreply.github.com>
2026-05-09 15:18:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (87.65%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,31 +1,31 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int temp[] = new int[n+m];
         int i=0;
         int j=0;
-        int k =0;
-        int ans[] = new int[n+m];
-        while(i < m && j < n){
+        int k=0;
+        while(i<m && j<n){
             if(nums1[i] < nums2[j]){
-                ans[k] = nums1[i];
+                temp[k] = nums1[i];
                 i++;
             }else{
-                ans[k] = nums2[j];
+                temp[k] = nums2[j];
                 j++;
             }
             k++;
         }
-        while(i < m){
-            ans[k] = nums1[i];
+        while(i<m){
+            temp[k] = nums1[i];
             i++;
             k++;
         }
-        while(j < n){
-            ans[k] = nums2[j];
+        while(j<n){
+            temp[k] = nums2[j];
             j++;
             k++;
         }
-        for(int p=0 ,l = 0; (p<nums1.length) && (l <ans.length); p++ , l++){
-            nums1[p] = ans[l];
+        for(int p=0,l=0 ; (p<temp.length && l<nums1.length); p++,l++){
+            nums1[l] = temp[p];
         }
     }
 }
